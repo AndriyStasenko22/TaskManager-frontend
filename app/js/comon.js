4a6e893c4fccd037b1a42f7fc620c7afbf19e95d
andriy <andriystasenko22@gmail.com>
2018-01-23 18:03:34 +0200

    Same

--- app/js/comon.js
+++ app/js/comon.js
@@ -1,2 +1,6 @@
 $(document).ready(function() {
+
+    $('.sidebar-menu .sidebar-menu-item-title').on('click', function () {
+       $(this).parent('.sidebar-menu-item-block').toggleClass('open');
+    });
 });
